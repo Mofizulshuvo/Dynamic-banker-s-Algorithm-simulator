6d558c8b5fc056cf336f664ffd595b13c3a962f6
Mofizul Islam Shuvo <mofizul.shuvoislam@gmail.com>
2026-06-19 14:16:37 +0600

    dynamic banker's algorithm simulation

---
+++ banker.cpp
@@ -0,0 +1,81 @@
+#include "banker.h"
+#include <iostream>
+using namespace std;
+
+void calculateNeed(vector<vector<int>> &need,
+                   vector<vector<int>> max,
+                   vector<vector<int>> alloc,
+                   int p, int r)
+{
+    for(int i = 0; i < p; i++)
+    {
+        for(int j = 0; j < r; j++)
+        {
+            need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
+}
+
+void printMatrix(vector<vector<int>> mat, int p, int r, string name)
+{
+    cout << "\n" << name << " Matrix:\n";
+    for(int i = 0; i < p; i++)
+    {
+        for(int j = 0; j < r; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool isSafe(vector<vector<int>> alloc,
+            vector<vector<int>> max,
+            vector<int> avail,
+            int p, int r)
+{
+    vector<vector<int>> need(p, vector<int>(r));
+    calculateNeed(need, max, alloc, p, r);
+
+    vector<bool> finish(p, false);
+    vector<int> work = avail;
+
+    int count = 0;
+
+    while(count < p)
+    {
+        bool found = false;
+
+        for(int i = 0; i < p; i++)
+        {
+            if(!finish[i])
+            {
+                bool canRun = true;
+
+                for(int j = 0; j < r; j++)
+                {
+                    if(need[i][j] > work[j])
+                    {
+                        canRun = false;
+                        break;
+                    }
+                }
+
+                if(canRun)
+                {
+                    for(int j = 0; j < r; j++)
+                        work[j] += alloc[i][j];
+
+                    finish[i] = true;
+                    found = true;
+                    count++;
+                }
+            }
+        }
+
+        if(!found)
+            return false;
+    }
+
+    return true;
+}
---
+++ fault.h
@@ -0,0 +1,10 @@
+
+#ifndef FAULT_H
+#define FAULT_H
+
+#include <vector>
+using namespace std;
+
+void injectFault(vector<int> &avail);
+
+#endif
